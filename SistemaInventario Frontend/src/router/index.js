946850800cb7dd29486f9be0096ff51888a7eae2
EJCP3 <euddycabrera03@gmail.com>
2025-12-03 15:01:58 -0400

    arreglo de router

--- SistemaInventario Frontend/src/router/index.js
+++ SistemaInventario Frontend/src/router/index.js
@@ -1,53 +1,41 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import DashboardView from '../views/DashboardView.vue'
 import ProductsList from '../views/products/ProductsList.vue'
-import ProductForm from '../views/products/ProductForm.vue'
-
-// MOVIMIENTOS
 import MovementsList from '../views/movements/MovementsList.vue'
 import MovementsForm from '../views/movements/MovementsForm.vue'
-
-// CATEGORÍAS
 import CategoriesList from '../views/categories/CategoriesList.vue'
-import CategoriesForm from '../views/categories/CategoriesForm.vue'
-
-// PROVEEDORES
 import SuppliersList from '../views/suppliers/SuppliersList.vue'
-
-// REPORTES
 import ReportsView from '../views/reports/ReportsView.vue'
-
-// LOGIN
 import LoginView from '../views/LoginView.vue'
 import ProductForm from '../views/products/ProductForm.vue'  
+import Home from '../views/Home.vue'
+import MainLayout from '../layouts/MainLayout.vue'
+
+
 
 const routes = [
-  { path: '/', component: DashboardView, name: 'Dashboard' },
-
-  // PRODUCTOS
-  { path: '/products', component: ProductsList, name: 'Products' },
-
-  { 
-    path: '/products/create',
-    name: 'ProductCreate',
-    component: ProductForm  
+  {
+    path: "/login",
+    component: LoginView,
+    name: "Login",
   },
 
-  // MOVIMIENTOS
-  { 
-    path: '/movements', 
-    component: MovementsList, 
-    name: 'Movements' 
-  },
+  {
+    path: "/",
+    component: MainLayout,
+    // meta: { requiresAuth: true },
+    children: [
+      { path: "", component: Home, name: "Home" },
 
-  { 
-    path: '/movements/create',
-    name: 'MovementCreate',
-    component: MovementsForm
-  },
 
-  // CATEGORÍAS
-  { path: '/categories', component: CategoriesList, name: 'Categories' },
+      { path: "products", component: ProductsList, name: "Products" },
+      { path: "products/create", component: ProductForm, name: "ProductCreate" },
+
+
+      { path: "movements", component: MovementsList, name: "Movements" },
+      { path: "movements/create", component: MovementsForm, name: "MovementCreate" },
+
+     
+      { path: "categories", component: CategoriesList, name: "Categories" },
 
      
       { path: "suppliers", component: SuppliersList, name: "Suppliers" },
