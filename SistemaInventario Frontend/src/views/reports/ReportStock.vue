d37fcfbcceaa63c14ef69d667626e59851ce79bc
EJCP3 <euddycabrera03@gmail.com>
2025-12-05 11:18:18 -0400

    lógica de reportStock

--- SistemaInventario Frontend/src/views/reports/ReportStock.vue
+++ SistemaInventario Frontend/src/views/reports/ReportStock.vue
@@ -1,11 +1,14 @@
 <script setup>
 import { ref, computed, onMounted } from "vue";
 import { useExistenciasStore } from "../../store/existencias";
+import { storeToRefs } from "pinia";
+
+const store = useExistenciasStore();
+const { existencias, loading } = storeToRefs(store);
+const { fetchExistencias } = store;
 
 const buscar = ref("");
 
-const store = useExistenciasStore();
-const { existencias, fetchExistencias, loading } = store;
 
 // ---------------------------
 // Lógica para estado del stock
@@ -27,7 +30,9 @@ const styleEstado = {
 // ---------------------------
 onMounted(() => {
   fetchExistencias();
-});
+}); 
+
+console.log(existencias)
 
 // ---------------------------
 // Crear lista procesada + estado + filtro
@@ -37,27 +42,38 @@ const productos = computed(() => {
 
   return existencias.value
     .map((item) => ({
-      id: item.id,
-      nombre: item.nombreProducto,
-      categoria: item.categoriaNombre,
-      stock: item.stockActual,
-      minimo: item.stockMinimo,
-      estado: calcularEstado(item.stockActual, item.stockMinimo),
+      id: item.ExistenciaId,
+      codigoSku: item.CodigoSku,
+      nombre: item.ProductoNombre,
+      almacen: item.AlmacenNombre,
+      stock: item.CantidadActual,
+      minimo: item.StockMinimo,
+      estado: calcularEstado(item.CantidadActual, item.StockMinimo),
     }))
     .filter((p) =>
       p.nombre.toLowerCase().includes(buscar.value.toLowerCase())
     );
 });
+
+
+console.log(productos)
 </script>
 
 <template>
   <header class="flex justify-between mt-5 mb-3">
-    <FormKit
-      type="search"
-      placeholder="Search..."
-      label="Buscar productos"
-      v-model="buscar"
-    />
+  <FormKit
+  type="search"
+  v-model="buscar"
+  placeholder="Buscar por nombre..."
+  :classes="{
+    outer: 'w-full max-w-sm',
+    label: 'text-sm font-semibold text-gray-600 mb-1',
+    input:
+      'input input-bordered w-full rounded-xl pl-10 bg-base-100 focus:outline-none focus:ring-2 focus:ring-primary',
+  }"
+>
+  <
+</FormKit>
 
     <button class="btn btn-accent ml-2">
       <img src="/IconExport.svg" class="w-5 mr-2" />
@@ -72,27 +88,27 @@ const productos = computed(() => {
           <tr>
             <th>ID</th>
             <th>Nombre</th>
-            <th>Categorías</th>
+            <th>Almacen</th>
             <th>Stock</th>
             <th>Mínimo</th>
             <th>Estado</th>
           </tr>
         </thead>
 
-        <tbody>
-          <tr
-            v-for="p in productos"
-            :key="p.id"
-            :class="styleEstado[p.estado]"
-          >
-            <th>{{ p.id }}</th>
-            <td>{{ p.nombre }}</td>
-            <td>{{ p.categoria }}</td>
-            <td>{{ p.stock }}</td>
-            <td>{{ p.minimo }}</td>
-            <td>{{ p.estado }}</td>
-          </tr>
-        </tbody>
+       <tbody>
+  <tr
+    v-for="p in productos"
+    :key="p.id"
+    :class="styleEstado[p.estado]"
+  >
+    <th>{{ p.codigoSku }}</th>
+    <td>{{ p.nombre }}</td>
+    <td>{{ p.almacen }}</td>
+    <td>{{ p.stock }}</td>
+    <td>{{ p.minimo }}</td>
+    <td>{{ p.estado }}</td>
+  </tr>
+</tbody>
       </table>
     </div>
   </section>
