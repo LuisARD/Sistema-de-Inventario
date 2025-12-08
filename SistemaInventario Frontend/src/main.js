0f21bbdc789782ad9597c7352ff7161da2344565
EJCP3 <euddycabrera03@gmail.com>
2025-12-08 09:20:35 -0400

    Mejorar en la validación

--- SistemaInventario Frontend/src/main.js
+++ SistemaInventario Frontend/src/main.js
@@ -5,15 +5,32 @@ import router from './router'
 import { createPinia } from 'pinia'
 import { plugin, defaultConfig } from '@formkit/vue'
 
-
 const app = createApp(App)
 app.use(createPinia())
-app.use(plugin, defaultConfig({
-    config:{
-        classes:{
-             message: "text-red-500 text-sm mt-1 font-bold"
+app.use(
+  plugin,
+  defaultConfig({
+    locales: {
+      es: {
+        ui: {
+          incomplete: "Por favor complete todos los campos correctamente."
         }
+      }
+    },
+    locale: "es", // activa la locale por defecto
+    config: {
+      classes: {
+        message: "text-red-500 text-sm mt-1 font-bold"
+      },
+      form: {
+        generateMessage: ({ node }) => {
+          if (node.props.type === "form") {
+            return "Por favor complete todos los campos correctamente.";
+          }
+        },
+      }
     }
-}))
+  })
+)
 app.use(router)
 app.mount('#app')
